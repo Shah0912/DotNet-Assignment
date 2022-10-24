f8ae8189482873b7d062171be9652db0976d3e05
Ganga Nair <45668012+GangaSuresh@users.noreply.github.com>
2022-10-24 23:42:41 +0530

    Add files via upload

---
+++ Day1- Ganga Nair/Assignment/Assignment-Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Calculator calculator=new Calculator();
+            do
+            {
+                calculator.ValidateData();
+            Repeat:
+                Console.WriteLine("What operation you want to perform \n1.Addition\n2.Comparision\n3.Swapping\n4.Print Table\nEnter you option: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (!calculator.Calculate(choice))
+                {
+                    goto Repeat;
+                }
+                string ch2;
+                Console.WriteLine("Any More Operations to perform (y|n): ");
+                ch2 = Console.ReadLine();
+                if (ch2 != "y")
+                {
+                    break;
+                }
+            }while(true);
+        }
+    }
+}
