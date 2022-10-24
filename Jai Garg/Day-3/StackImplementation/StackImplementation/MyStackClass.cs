330adc993bef18043deafaef4d98da3aee7bc53a
jaigarg <42888030+jaigarg@users.noreply.github.com>
2022-10-25 03:39:55 +0530

    Added Day 3 Work

---
+++ Jai Garg/Day-3/StackImplementation/StackImplementation/MyStackClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementation
+{
+    public class MyStackClass
+    {
+        private ArrayList elements = new ArrayList();
+        public bool IsEmpty
+        {
+            get
+            {
+                return (elements.Count == 0);
+            }
+        }
+        public object Push(object element)
+        {
+            elements.Insert(0, element);
+            return elements[0];
+        }
+        public object Pop()
+        {
+            object top = Top();
+            elements.RemoveAt(0);
+            return top;
+        }
+        public object Top()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is Empty");
+            return elements[0];
+        }
+    }
+
+}
