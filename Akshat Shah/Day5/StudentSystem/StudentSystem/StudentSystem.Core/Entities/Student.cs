0cc99141e0082088e3260a25cc0237371359db46
Akshat Shah <akshatshah_21@yahoo.in>
2022-11-01 09:22:32 +0530

    Add Student API, Core, Infrastructure

---
+++ Akshat Shah/Day5/StudentSystem/StudentSystem/StudentSystem.Core/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentSystem.Core.Entities
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Name must not exceed 50 characters")]
+        public string Name { get; set; }
+
+        [Range(0.0, 10.0, ErrorMessage = "Gpa must be between 0.0 and 10.0")]
+        public float Gpa { get; set; }
+
+        [Required]
+        // [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Email address not valid")]
+        [EmailAddressAttribute]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Akshat Shah/Day5/StudentSystem/StudentSystem/StudentSystem/Controllers/StudentsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentSystem.Core.Entities;
+using StudentSystem.Infrastructure.Data;
+
+namespace StudentSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+        private readonly IStudentRepository _repository;
+
+        public StudentsController(StudentDbContext context, IStudentRepository repository)
+        {
+            _context = context;
+            _repository = repository;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+            // return await _repository.GetAll();
+        }
+
+        // GET: api/Students/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var student = await _repository.Get(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/Students/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            await _repository.Update(student);
+
+            return NoContent();
+        }
+
+        // POST: api/Students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            student = await _repository.Add(student);
+            if (student == null)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Could not update student");
+            }
+            return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        // DELETE: api/Students/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            await _repository.Delete(id);
+
+            return NoContent();
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.Id == id);
+        }
+    }
+}
